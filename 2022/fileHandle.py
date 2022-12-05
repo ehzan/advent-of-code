70e57a2fa2eea8efd9411052ed0667c5f1f2bf85
ehzan <enyzadi@gmail.com>
2022-12-05 11:12:39 +0330

    refactor

--- 2022/fileHandle.py
+++ 2022/fileHandle.py
@@ -2,7 +2,7 @@ import re
 
 
 def readfile(filename):
-    path = re.sub(r'[^\\]+$', filename, __file__)
+    path = re.sub(r'[^\\]+$', 'input\\\\' + filename, __file__)
     with open(path, 'r', encoding='UTF-8') as f:
-        filedata = f.read()
-    return filedata[:-1]
+        data = f.read()
+    return data[:-1]
