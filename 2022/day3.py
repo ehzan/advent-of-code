0ef47f81a218825410a969811e094dccf81256df
ehzan <enyzadi@gmail.com>
2022-12-14 14:53:17 +0330

    2022 day3 refactored

--- 2022/day3.py
+++ 2022/day3.py
@@ -1,9 +1,8 @@
-from fileHandle import readfile
-from itertools import product
+import fileHandle, itertools
 
 
-def puzzle5(input_file='day3.txt'):
-    data = readfile(input_file).split('\n')
+def puzzle5(input_file):
+    data = fileHandle.readfile(input_file).splitlines()
     items = []
     for rucksack in data:
         n = int(len(rucksack) / 2)
@@ -18,22 +17,26 @@ def puzzle5(input_file='day3.txt'):
                 continue
             break
 
-    p_sum = 0
-    for item in items:
-        p_sum += ord(item.lower()) - ord('a') + (27 if item.isupper() else 1)
-    return p_sum
+    _sum = 0
+    for l in items:
+        _sum += ord(l.lower()) - ord('a') + (27 if l.isupper() else 1)
+    return _sum
 
 
-def puzzle6(input_file='day3.txt'):
-    data = readfile(input_file).split('\n')
+def puzzle6(input_file):
+    data = fileHandle.readfile(input_file).splitlines()
     items = []
     for i in range(0, len(data), 3):
-        for x, y, z in product(data[i], data[i + 1], data[i + 2]):
+        for x, y, z in itertools.product(data[i], data[i + 1], data[i + 2]):
             if x == y and y == z:
                 items.append(x)
                 break
-                
-    p_sum = 0
-    for item in items:
-        p_sum += ord(item.lower()) - ord('a') + (27 if item.isupper() else 1)
-    return p_sum
+
+    _sum = 0
+    for l in items:
+        _sum += ord(l.lower()) - ord('a') + (27 if l.isupper() else 1)
+    return _sum
+
+
+print('Day #3, Part One:', puzzle5('day3.txt'))
+print('Day #3, Part Two:', puzzle6('day3.txt'))
