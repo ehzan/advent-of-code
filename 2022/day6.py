97b3dfc85258007e840bed1519f322ef18922c76
ehzan <enyzadi@gmail.com>
2022-12-14 15:12:29 +0330

    2022 day6 refactored

--- 2022/day6.py
+++ 2022/day6.py
@@ -1,18 +1,22 @@
-from fileHandle import readfile
+import fileHandle
 
 
-def puzzle11(input_file='day6.txt'):
-    data = readfile(input_file)
+def puzzle11(input_file):
+    data = fileHandle.readfile(input_file)
     for i in range(4, len(data) + 1):
         if len(set(data[i - 4:i])) == 4:
             return i
     return None
 
 
-def puzzle12(input_file='day6.txt'):
-    data = readfile(input_file)
+def puzzle12(input_file):
+    data = fileHandle.readfile(input_file)
     s = puzzle11(input_file)
     for i in range(s + 14, len(data) + 1):
         if len(set(data[i - 14:i])) == 14:
             return i
     return None
+
+
+print('Day #6, Part One:', puzzle11('day6.txt'))
+print('Day #6, Part Two:', puzzle12('day6.txt'))
