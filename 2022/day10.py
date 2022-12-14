933f16622018837c2556536ec3ba44ac9df83bff
ehzan <enyzadi@gmail.com>
2022-12-14 15:13:22 +0330

    2022 day10 refactored

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,7 +1,7 @@
 import fileHandle
 
 
-def puzzle19(input_file='day10.txt'):
+def puzzle19(input_file):
     data = fileHandle.readfile(input_file)
     instructions = [line for line in data.splitlines()]
     cycle = 1
@@ -9,30 +9,32 @@ def puzzle19(input_file='day10.txt'):
     _sum = 0
     for instruction in instructions:
         cycle += 1
-        if cycle in [20, 60, 100, 140, 180, 220]: _sum += cycle * x
+        if cycle in [20, 60, 100, 140, 180, 220]:
+            _sum += cycle * x
         if instruction[:4] == 'addx':
             cycle += 1
             x = x + int(instruction.split(' ')[1])
-            if cycle in [20, 60, 100, 140, 180, 220]: _sum += cycle * x
+            if cycle in [20, 60, 100, 140, 180, 220]:
+                _sum += cycle * x
     return _sum
 
 
-def draw(screen, pixel, x):
+def draw(pixel, screen, x):
     screen[pixel] = '#' if pixel % 40 in [x - 1, x, x + 1] else '.'
 
 
-def puzzle20(input_file='day10.txt'):
+def puzzle20(input_file):
     data = fileHandle.readfile(input_file)
     instructions = [line for line in data.splitlines()]
     screen = ['*'] * 240
     cycle = 1
     x = 1
-    draw(screen, cycle - 1, x)
+    draw(cycle - 1, screen, x)
     for instruction in instructions:
-        draw(screen, cycle - 1, x)
+        draw(cycle - 1, screen, x)
         cycle += 1
         if instruction[:4] == 'addx':
-            draw(screen, cycle - 1, x)
+            draw(cycle - 1, screen, x)
             cycle += 1
             x = x + int(instruction.split(' ')[1])
 
@@ -41,3 +43,7 @@ def puzzle20(input_file='day10.txt'):
         if i + 1 in [40, 80, 120, 160, 200, 240]:
             print()
     return 'RZHFGJCB'
+
+
+print('Day #10, Part One:', puzzle19('day10.txt'))
+print('Day #10, Part Two:', puzzle20('day10.txt'))
