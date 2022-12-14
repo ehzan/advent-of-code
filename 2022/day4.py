848e9d6c01f719e93741f59a17f3dd47b6054149
ehzan <enyzadi@gmail.com>
2022-12-14 14:53:30 +0330

    2022 day4 refactored

--- 2022/day4.py
+++ 2022/day4.py
@@ -1,25 +1,25 @@
-from fileHandle import readfile
+import fileHandle, re
 
 
-def puzzle7(input_file='day4.txt'):
-    data = readfile(input_file).split('\n')
-
-    pairs = [d.split(',') for d in data]
-    pairs = [[ids.split('-') for ids in pair] for pair in pairs]
+def puzzle7(input_file):
+    data = fileHandle.readfile(input_file).splitlines()
+    pairs = [re.split(r',|-', pair) for pair in data]
     cnt = 0
-    for elf in pairs:
-        cnt += (int(elf[0][0]) <= int(elf[1][0])) and (int(elf[0][1]) >= int(elf[1][1])) or \
-               (int(elf[0][0]) >= int(elf[1][0])) and (int(elf[0][1]) <= int(elf[1][1]))
+    for sections in pairs:
+        cnt += int(sections[0]) <= int(sections[2]) and int(sections[3]) <= int(sections[1]) or \
+               int(sections[2]) <= int(sections[0]) and int(sections[1]) <= int(sections[3])
     return cnt
 
 
-def puzzle8(input_file='day4.txt'):
-    data = readfile(input_file).split('\n')
-
-    pairs = [d.split(',') for d in data]
-    pairs = [[ids.split('-') for ids in pair] for pair in pairs]
+def puzzle8(input_file):
+    data = fileHandle.readfile(input_file).splitlines()
+    pairs = [re.split(r',|-', pair) for pair in data]
     cnt = 0
-    for elf in pairs:
-        cnt += (int(elf[0][0]) <= int(elf[1][0])) and (int(elf[0][1]) >= int(elf[1][0])) or \
-               (int(elf[0][0]) >= int(elf[1][0])) and (int(elf[0][0]) <= int(elf[1][1]))
+    for sections in pairs:
+        cnt += int(sections[0]) <= int(sections[2]) <= int(sections[1]) or \
+               int(sections[2]) <= int(sections[0]) <= int(sections[3])
     return cnt
+
+
+print('Day #4, Part One:', puzzle7('day4.txt'))
+print('Day #4, Part Two:', puzzle8('day4.txt'))
