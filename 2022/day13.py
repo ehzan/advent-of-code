7ffee71cc09c500d126b700fd957e2797f727c9b
ehzan <enyzadi@gmail.com>
2022-12-14 16:17:48 +0330

    2022 day13 refactored

--- 2022/day13.py
+++ 2022/day13.py
@@ -5,35 +5,38 @@ def compare(l1, l2):
     # print(l1, l2, l1.__class__, l2.__class__)
     if isinstance(l1, int) and isinstance(l2, int):
         return 1 if l1 < l2 else -1 if l1 > l2 else 0
-    elif isinstance(l1, int):
-        return compare([l1], l2)
-    elif isinstance(l2, int):
-        return compare(l1, [l2])
+    if isinstance(l1, int):  # and l2 is list
+        l1 = [l1]
+    if isinstance(l2, int):  # and l1 is list
+        l2 = [l2]
     for i in range(min(len(l1), len(l2), )):
         cmp = compare(l1[i], l2[i])
-        if cmp: return cmp
+        if cmp:  # i.e. cmp==1 or -1
+            return cmp
     return 1 if len(l1) < len(l2) else -1 if len(l1) > len(l2) else 0
 
 
-def puzzle25(input_file='day13.txt'):
+def puzzle25(input_file):
     data = fileHandle.readfile(input_file).split('\n\n')
-    right_order = []
+    right_orders = []
     for i in range(len(data)):
-        pair = data[i].splitlines()
-        packet1 = eval(pair[0])
-        packet2 = eval(pair[1])
+        packet1, packet2 = map(eval, data[i].splitlines())
         if compare(packet1, packet2) == 1:
-            right_order.append(i + 1)
+            right_orders.append(i + 1)
     # print(right_order)
-    return sum(right_order)
+    return sum(right_orders)
 
 
-def puzzle26(input_file='day13.txt'):
+def puzzle26(input_file):
     data = fileHandle.readfile(input_file)
     data = re.split(r'\n+', data)
-    packets = [eval(d) for d in data]
+    packets = list(map(eval, data))  # i.e. packets=[eval(d) for d in data]
     packets.append([[2]])
     packets.append([[6]])
     packets.sort(key=functools.cmp_to_key(compare), reverse=True)
     # print(*packets, sep='\n')
     return (packets.index([[2]]) + 1) * (packets.index([[6]]) + 1)
+
+
+print('Day #13, Part One:', puzzle25('day13.txt'))
+print('Day #13, Part Two:', puzzle26('day13.txt'))
