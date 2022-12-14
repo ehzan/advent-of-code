38952dd009184b29fba9bb0001614499d9000660
ehzan <enyzadi@gmail.com>
2022-12-14 14:53:06 +0330

    2022 day2 refactored

--- 2022/day2.py
+++ 2022/day2.py
@@ -1,8 +1,8 @@
-from fileHandle import readfile
+import fileHandle
 
 
-def puzzle3(input_file='day2.txt'):
-    data = readfile(input_file).split('\n')
+def puzzle3(input_file):
+    data = fileHandle.readfile(input_file).splitlines()
     score = 0
     for c in data:
         score += ord(c[2]) - ord('X') + 1
@@ -14,15 +14,19 @@ def puzzle3(input_file='day2.txt'):
     return score
 
 
-def puzzle4(input_file='day2.txt'):
-    data = readfile(input_file).split('\n')
+def puzzle4(input_file):
+    data = fileHandle.readfile(input_file).splitlines()
     score = 0
     for c in data:
         match c[2]:
             case 'X':
-                score += 3 if c[0] == 'A' else ord(c[0]) - ord('A')
+                score += ord(c[0]) - ord('A') if c[0] != 'A' else 3
             case 'Y':
                 score += 3 + ord(c[0]) - ord('A') + 1
             case 'Z':
-                score += 6 + (1 if c[0] == 'C' else ord(c[0]) - ord('A') + 2)
+                score += 6 + (ord(c[0]) - ord('A') + 2 if c[0] != 'C' else 1)
     return score
+
+
+print('Day #2, Part One:', puzzle3('day2.txt'))
+print('Day #2, Part Two:', puzzle4('day2.txt'))
