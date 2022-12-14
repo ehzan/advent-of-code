0d02a53a25fdedf94fc09293a3c75dc72add1ceb
ehzan <enyzadi@gmail.com>
2022-12-14 15:13:00 +0330

    2022 day8 refactored

--- 2022/day8.py
+++ 2022/day8.py
@@ -1,7 +1,7 @@
 import fileHandle
 
 
-def puzzle15(input_file='day8.txt'):
+def puzzle15(input_file):
     data = fileHandle.readfile(input_file)
     h = [[int(ch) for ch in line] for line in data.splitlines()]
     visible = 2 * (len(h) + len(h[0])) - 4
@@ -9,14 +9,12 @@ def puzzle15(input_file='day8.txt'):
         for x in range(1, len(h[y]) - 1):
             row = h[y]
             col = [h[i][x] for i in range(len(h))]
-            visible += h[y][x] > min([max(row[:x]), max(row[x + 1:]), max(col[:y]), max(col[y + 1:])])
+            visible += h[y][x] > min(max(row[:x]), max(row[x + 1:]), max(col[:y]), max(col[y + 1:]))
     return visible
 
 
-def puzzle16(input_file='day8.txt'):
-    data = fileHandle.readfile(input_file)
-    h = [[int(ch) for ch in line] for line in data.splitlines()]
-    score = [[0] * len(h[0]) for y in range(len(h), )]
+def calculate_scores(h):
+    scores = [[0] * len(h[0]) for y in range(len(h), )]
     for y in range(1, len(h) - 1):
         for x in range(1, len(h[y]) - 1):
             row = h[y]
@@ -30,9 +28,17 @@ def puzzle16(input_file='day8.txt'):
                 yu -= 1
             while yd < len(col) - 1 and col[yd] < h[y][x]:
                 yd += 1
-            score[y][x] = (x - xl) * (xr - x) * (y - yu) * (yd - y)
-    max_score = 0
-    for y in range(len(h)):
-        for x in range(len(h[y])):
-            max_score = max(max_score, score[y][x])
-    return max_score
+            scores[y][x] = (x - xl) * (xr - x) * (y - yu) * (yd - y)
+    return scores
+
+
+def puzzle16(input_file):
+    data = fileHandle.readfile(input_file)
+    h = [[int(ch) for ch in line] for line in data.splitlines()]
+    scores = calculate_scores(h)
+    max_score_of_rows = map(max, scores)
+    return max(max_score_of_rows)
+
+
+print('Day #8, Part One:', puzzle15('day8.txt'))
+print('Day #8, Part Two:', puzzle16('day8.txt'))
