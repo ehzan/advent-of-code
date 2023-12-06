89270f7f5adb3d74fc4c849c0e9a8047ae4632d8
ehsan <enyazdi@gmail.com>
2023-12-06 10:01:57 +0330

    2022 day9 refactored

--- 2022/day9.py
+++ 2022/day9.py
@@ -1,52 +1,64 @@
-import fileHandle
+import file_handle
+
+VECTORS = {'L': (-1, 0), 'R': (1, 0), 'U': (0, -1), 'D': (0, 1), }
 
 
-def shift(node, direction):
-    vector = {'L': [-1, 0], 'R': [1, 0], 'U': [0, -1], 'D': [0, 1]}
-    return [node[0] + vector[direction][0], node[1] + vector[direction][1]]
+def shift(point: tuple[int, int], direction: chr) -> tuple[int, int]:
+    (x, y) = point
+    (dx, dy) = VECTORS[direction]
+    return x + dx, y + dy
 
 
-def pull(head, tail):
-    if abs(head[0] - tail[0]) <= 1 and abs(head[1] - tail[1]) <= 1:
+def pull(head: tuple[int, int], tail: tuple[int, int], ) -> tuple[int, int]:
+    (Hx, Hy), (Tx, Ty) = head, tail
+    if abs(Hx - Tx) <= 1 and abs(Hy - Ty) <= 1:
         return tail
 
-    if tail[1] == head[1]:
-        tail[0] = (head[0] + tail[0]) // 2
-    elif tail[0] == head[0]:
-        tail[1] = (head[1] + tail[1]) // 2
+    (x, y) = tail
+    if Ty == Hy:
+        x = (Hx + Tx) // 2
+    elif Tx == Hx:
+        y = (Hy + Ty) // 2
     else:
-        tail[0] = (head[0] + tail[0]) // 2 if abs(head[0] - tail[0]) > 1 else head[0]
-        tail[1] = (head[1] + tail[1]) // 2 if abs(head[1] - tail[1]) > 1 else head[1]
-    return tail
+        x = (Hx + Tx) // 2 if abs(Hx - Tx) > 1 else Hx
+        y = (Hy + Ty) // 2 if abs(Hy - Ty) > 1 else Hy
+
+    return x, y
 
 
-def puzzle17(input_file):
-    data = fileHandle.readfile(input_file)
-    steps = [(line.split(' ')[0], int(line.split(' ')[1])) for line in data.splitlines()]
-    H = [0, 0]
-    T = [0, 0]
-    visited = {(T[0], T[1])}
-    for step in steps:
-        for i in range(step[1]):
-            H = shift(H, step[0])
-            pull(H, T)
-            visited.add((T[0], T[1]))
-    return len(set(visited))
+def puzzle17(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+    moves = [{'direction': row.split(' ')[0], 'steps': int(row.split(' ')[1])}
+             for row in data.splitlines()]
+
+    head = tail = (0, 0)
+    visited = {tail}
+    for move in moves:
+        for _ in range(move['steps']):
+            head = shift(head, move['direction'])
+            tail = pull(head, tail)
+            visited.add(tail)
+
+    return len(visited)
 
 
-def puzzle18(input_file):
-    data = fileHandle.readfile(input_file)
-    steps = [(line.split(' ')[0], int(line.split(' ')[1])) for line in data.splitlines()]
-    rope = [[0, 0] for i in range(10)]
-    visited = {(rope[9][0], rope[9][1])}
-    for step in steps:
-        for i in range(step[1]):
-            rope[0] = shift(rope[0], step[0])
-            for j in range(1, 10):
-                pull(rope[j - 1], rope[j])
-            visited.add((rope[9][0], rope[9][1]))
-    return len(set(visited))
+def puzzle18(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+    moves = [{'direction': row.split(' ')[0], 'steps': int(row.split(' ')[1])}
+             for row in data.splitlines()]
+
+    rope = [(0, 0)] * 10
+    visited = {rope[-1]}
+    for move in moves:
+        for _ in range(move['steps']):
+            rope[0] = shift(rope[0], move['direction'])
+            for i in range(1, 10):
+                rope[i] = pull(rope[i - 1], rope[i])
+            visited.add(rope[-1])
+
+    return len(visited)
 
 
-print('Day #9, Part One:', puzzle17('day9.txt'))
-print('Day #9, Part Two:', puzzle18('day9.txt'))
+if __name__ == '__main__':
+    print('Day #9, part one:', puzzle17('./input/day9.txt'))
+    print('Day #9, part two:', puzzle18('./input/day9.txt'))
