b834a3c867875527bdbf5cbcd3238972f209a9ee
ehsan <enyazdi@gmail.com>
2023-12-07 10:52:02 +0330

    shell_sort added

--- 2023/sort.py
+++ 2023/sort.py
@@ -25,3 +25,51 @@ def insertion_sort(data: list):
         while 0 <= (j := j - 1) and ai < data[j]:
             data[j + 1] = data[j]
         data[j + 1] = ai
+
+
+def selection_sort(data: list):
+    for i in range(len(data)):
+        idx_min = i
+        for j in range(i + 1, len(data)):
+            if data[j] < data[idx_min]:
+                idx_min = j
+        data[i], data[idx_min] = data[idx_min], data[i]
+
+
+def shell_sort(data: list):
+    for gap in [701, 301, 132, 57, 23, 10, 4, 1]:  # Ciura gap sequence
+        for i in range(gap, len(data)):
+            j, ai = i, data[i]
+            while 0 <= (j := j - gap) and ai < data[j]:
+                data[j + gap], data[j] = data[j], data[j + gap]
+            data[j + gap] = ai
+
+
+def test():
+    with open('./input/sort-data.txt', 'r', encoding='UTF-8') as f:
+        sort_data = f.read().strip()
+
+    the_data = list(map(int, sort_data.splitlines()))
+    test_data = the_data.copy()
+    test_data.sort()
+    sys.setrecursionlimit(len(the_data))
+
+    print('sorting algorithm \truntime(random / sorted data)')
+    for func in [list.sort, exchange_sort, bubble_sort, insertion_sort, selection_sort,
+                 shell_sort, ]:
+        data = the_data.copy()
+
+        t = timeit.timeit(stmt='func(data)', globals=locals(), number=1)
+        print(f" •{func.__name__}    \t\t {round(t, 3)}", end='\t ')
+
+        for i in range(0, len(test_data), 10000):
+            assert data[i] == test_data[i]
+
+        t = timeit.timeit(stmt='func(data)', globals=locals(), number=1)
+        print(round(t, 3))
+
+    print('(data size:', len(the_data), 'records)\n')
+
+
+if __name__ == '__main__':
+    test()
