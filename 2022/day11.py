8bdc178eb270bd6f811e77010c9ab3068f72de07
ehsan <enyazdi@gmail.com>
2023-12-08 11:23:26 +0330

    2022 day11 refactored

--- 2022/day11.py
+++ 2022/day11.py
@@ -1,74 +1,67 @@
-import fileHandle
+import math
+import re
+import time
+
+import file_handle
 
 
-def calculate(operand, operation):
-    operand2 = operation.split(' ')[1]
-    operand2 = operand if operand2 == 'old' else int(operand2)
-    if operation[0] == '+':
-        return operand + operand2
-    elif operation[0] == '*':
-        return operand * operand2
-    else:
-        return 'error'
-
-
-def read_monkey_attributes(data):
+def parse_input(data: str) -> list[dict]:
     monkeys = []
-    worry_level = []
-    item = 0
-    for p in data:
-        lines = p.splitlines()
-        id = int(lines[0][-2])
-        items = []
-        for level in lines[1].split('Starting items: ')[1].split(', '):
-            items.append(item)
-            worry_level.append(int(level))
-            item += 1
-        operation = lines[2].split('new = old ')[1]
-        test_divisor = int(lines[3].split('divisible by ')[1])
-        if_true = int(lines[4].split('throw to monkey ')[1])
-        if_false = int(lines[5].split('throw to monkey ')[1])
-        monkeys.append({'id': id, 'items': items, 'operation': operation, 'activity': 0,
-                        'test_divisor': test_divisor, 'if_true': if_true, 'if_false': if_false, })
-    return monkeys, worry_level
+    regex = (r'Monkey (\d+):\n' +
+             r'.* items: ([0-9, ]*)\n' +
+             r'.* new = (.*)\n' +
+             r'.* divisible by (\d+)\n' +
+             r'.* true: .* (\d+)\n' +
+             r'.* false: .* (\d+)(?:\n{2}|$)')
+    for (id_, items, op, divisor, if_true, if_false) in re.findall(regex, data):
+        monkeys.append({'id': int(id_),
+                        'activity': 0,
+                        'items': list(map(int, items.split(', '))),
+                        'operation': op,
+                        'test_divisor': int(divisor),
+                        'throw': {True: int(if_true), False: int(if_false), }
+                        })
+    return monkeys
 
 
-def puzzle21(input_file):
-    data = fileHandle.readfile(input_file).split('\n\n')
-    monkeys, worry_level = read_monkey_attributes(data)
-    for round in range(20):
+def puzzle21(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+    monkeys = parse_input(data)
+
+    for _ in range(20):
         for monkey in monkeys:
-            for item in monkey['items']:
+            for val in monkey['items']:
                 monkey['activity'] += 1
-                worry_level[item] = calculate(worry_level[item], monkey['operation']) // 3
-                receiver = monkey['if_true'] if worry_level[item] % monkey['test_divisor'] == 0 \
-                    else monkey['if_false']
-                monkeys[receiver]['items'].append(item)
+                old = val
+                new_val = eval(monkey['operation']) // 3
+                receiver = monkey['throw'][new_val % monkey['test_divisor'] == 0]
+                monkeys[receiver]['items'].append(new_val)
             monkey['items'].clear()
-    activities = sorted([monkey['activity'] for monkey in monkeys])
-    print(activities)
-    return activities[-1] * activities[-2]
+
+    activities = sorted([monkey['activity'] for monkey in monkeys], reverse=True)
+    return activities[0] * activities[1]
 
 
-def puzzle22(input_file):
-    data = fileHandle.readfile(input_file).split('\n\n')
-    monkeys, worry_level = read_monkey_attributes(data)
-    common_multiple = 1
-    for monkey in monkeys:
-        common_multiple *= monkey['test_divisor']
-    for round in range(10000):
+def puzzle22(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+    monkeys = parse_input(data)
+    lcm = math.lcm(*[monkey['test_divisor'] for monkey in monkeys])
+
+    for _ in range(10000):
         for monkey in monkeys:
-            for item in monkey['items']:
+            for val in monkey['items']:
                 monkey['activity'] += 1
-                worry_level[item] = calculate(worry_level[item], monkey['operation']) % common_multiple
-                receiver = monkey['if_true'] if worry_level[item] % monkey['test_divisor'] == 0 \
-                    else monkey['if_false']
-                monkeys[receiver]['items'].append(item)
+                old = val
+                new_val = eval(monkey['operation']) % lcm
+                receiver = monkey['throw'][new_val % monkey['test_divisor'] == 0]
+                monkeys[receiver]['items'].append(new_val)
             monkey['items'].clear()
-    activities = sorted([monkey['activity'] for monkey in monkeys])
-    print(activities)
-    return activities[-1] * activities[-2]
+
+    activities = sorted([monkey['activity'] for monkey in monkeys], reverse=True)
+    return activities[0] * activities[1]
 
 
-print('Day #11, Part One:', puzzle21('day11.txt'))
-print('Day #11, Part Two:', puzzle22('day11.txt'))
+if __name__ == '__main__':
+    print('Day #11, part one:', puzzle21('./input/day11.txt'))
+    timestamp = time.time()
+    print('Day #11, part two:', puzzle22('./input/day11.txt'), f' (runtime: {round(time.time() - timestamp, 1)}s) ')
