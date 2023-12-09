7a65d0e147635f60f9f2856d09af1662031052d1
ehsan <enyazdi@gmail.com>
2023-12-09 11:23:04 +0330

    2022 day14 refactored

--- 2022/day14.py
+++ 2022/day14.py
@@ -1,63 +1,65 @@
-import fileHandle
+import file_handle
+
+SOURCE_X, SOURCE_Y = 500, 0
 
 
-def scan_cave(data):
-    cave = [[0] * 1000 for i in range(1000)]
-    max_y = 0
-    for line in data:
-        points = line.split(' -> ')
-        for i in range(len(points) - 1):
-            x1, y1 = map(int, points[i].split(','))
-            x2, y2 = map(int, points[i + 1].split(','))
+def pares_input(data: str) -> list[list[int]]:
+    rocks = [[tuple(map(int, point.split(','))) for point in row.split(' -> ')]
+             for row in data.splitlines()]
+
+    max_y = max(point[1] for rock in rocks for point in rock)
+    cave = [[0] * (SOURCE_X + max_y + 3) for _ in range(max_y + 1)]
+    for rock in rocks:
+        for (x1, y1), (x2, y2) in zip(rock, rock[1:]):
             if x1 == x2:
                 for y in range(min(y1, y2), max(y1, y2) + 1):
-                    cave[x1][y] = 1
+                    cave[y][x1] = 1
             if y1 == y2:
                 for x in range(min(x1, x2), max(x1, x2) + 1):
-                    cave[x][y1] = 1
-            max_y = max(max_y, y1, y2)
-    return cave, max_y
+                    cave[y1][x] = 1
+
+    return cave
 
 
-def pour_sand(cave, max_y):
-    x, y = 500, 0
-    while y < max_y:
-        if cave[x][y + 1] == 0:
-            y += 1
-        elif cave[x - 1][y + 1] == 0:
-            x, y = x - 1, y + 1
-        elif cave[x + 1][y + 1] == 0:
-            x, y = x + 1, y + 1
+def pour_sand(cave: list[list[int]]) -> int:
+    sx, sy = SOURCE_X, SOURCE_Y
+    for sy in range(len(cave) - 1):
+        if not cave[sy + 1][sx]:
+            continue
+        elif not cave[sy + 1][sx - 1]:
+            sx -= 1
+        elif not cave[sy + 1][sx + 1]:
+            sx += 1
         else:
-            cave[x][y] = 1
-            break
-    return x, y
+            cave[sy][sx] = 1
+            return sy
+
+    return sy + 1
 
 
-def puzzle27(input_file):
-    data = fileHandle.readfile(input_file).splitlines()
-    cave, max_y = scan_cave(data)
-    sand = 0
-    y = 0
-    while y < max_y:
-        sand += 1
-        x, y = pour_sand(cave, max_y)
-    return sand - 1
+def puzzle27(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+    cave = pares_input(data)
+
+    sands = 0
+    while pour_sand(cave) < len(cave) - 1:
+        sands += 1
+
+    return sands
 
 
-def puzzle28(input_file):
-    data = fileHandle.readfile(input_file).splitlines()
-    cave, max_y = scan_cave(data)
-    max_y += 2
-    for x in range(len(cave)):
-        cave[x][max_y] = 1
-    sand = 0
-    y = True
-    while y:
-        sand += 1
-        x, y = pour_sand(cave, max_y)
-    return sand
+def puzzle28(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+    cave = pares_input(data)
+    cave.extend([[0] * len(cave[0]), [1] * len(cave[0])])
+
+    sands = 0
+    while pour_sand(cave) > SOURCE_Y:
+        sands += 1
+
+    return sands + 1
 
 
-print('Day #14, Part One:', puzzle27('day14.txt'))
-print('Day #14, Part Two:', puzzle28('day14.txt'))
+if __name__ == '__main__':
+    print('Day #14, part one:', puzzle27('./input/day14.txt'))
+    print('Day #14, part two:', puzzle28('./input/day14.txt'))
