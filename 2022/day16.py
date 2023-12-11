1a590527aebd4d9a0ead5c9a376fe91ee50bd8b0
ehsan <enyazdi@gmail.com>
2023-12-11 10:23:19 +0330

    2022 day16 refactored

--- 2022/day16.py
+++ 2022/day16.py
@@ -1,66 +1,110 @@
-import fileHandle, re
+import functools
+import re
+import time
+
+import file_handle
 
 
-def floyd_warshall(adjacents):
-    n = len(adjacents)
-    MAX_PATH_LENGTH = 30
-    _dist = [[MAX_PATH_LENGTH] * n for _ in range(n)]
-    for v1 in range(n):
-        for v2 in adjacents[v1]:
-            _dist[v1][v2] = 1
+def parse_input(data: str) -> list[dict]:
+    valve_specs = re.findall(r'^Valve (\w+).*=(\d+).* valves? (.*)$', data, re.MULTILINE)
+
+    _graph = [{'name': spec[0],
+               'flow_rate': int(spec[1]),
+               'adjacency': spec[2].split(', '), }
+              for spec in valve_specs]
+
+    index = {valve['name']: i for i, valve in enumerate(_graph)}
+    for node in _graph:
+        node['adjacency'] = {index[name] for name in node['adjacency']}
+
+    return _graph
+
+
+def floyd_warshall(graph: list[dict]) -> list[list[int]]:
+    infinity = 30
+    n = len(graph)
+    _dist = [[infinity] * n for _ in range(n)]
+
+    for v in range(n):
+        _dist[v][v] = 0
+        for u in graph[v]['adjacency']:
+            _dist[v][u] = 1
+
     for mid in range(n):
-        for v1 in range(n):
-            for v2 in range(n):
-                _dist[v1][v2] = min(_dist[v1][v2], _dist[v1][mid] + _dist[mid][v2])
+        for v in range(n):
+            for u in range(n):
+                _dist[v][u] = min(_dist[v][u], _dist[v][mid] + _dist[mid][u])
+
     return _dist
 
 
-def parse_input(input_file):
-    data = fileHandle.readfile(input_file)
-    valves_spec = [re.findall(r'[A-Z]{2}|\d+', line) for line in data.splitlines()]
-    valves = [spec[0] for spec in valves_spec]
-    _flow_rates = [int(spec[1]) for spec in valves_spec]
-    adjacents = [{valves.index(v) for v in spec[2:]} for spec in valves_spec]
-    _dist = floyd_warshall(adjacents)
-    return valves, _flow_rates, _dist
+def dfs(v: int, t: int, valves: frozenset[int]) -> int:
+    _max_flow = 0
+    dfs_stack = [(v, t, 0, valves,)]
+    while dfs_stack:
+        (v, t, p, remaining) = dfs_stack.pop()
+        _max_flow = max(_max_flow, p)
+        for u in remaining:
+            if (tt := t - dist[v][u] - 1) > 0:
+                pp = p + graph[u]['flow_rate'] * tt
+                dfs_stack.append((u, tt, pp, remaining - {u},))
+
+    return _max_flow
 
 
-def max_flow(v1, t1, closed_valves):
-    _max = 0
-    for v in closed_valves:
-        if t1 > dist[v1][v] + 1:
-            closed2 = closed_valves.copy()
-            closed2.remove(v)
-            t = t1 - dist[v1][v] - 1
-            p = flow_rates[v] * t + max_flow(v, t, closed2)
-            _max = max(_max, p)
-    return _max
+global graph, dist, v0, t0
 
 
-def dfs(cur, t1, closed_valves):
-    v1 = cur[-1]
-    if len(closed_valves - set(cur)) == 0:
-        dfs.i = dfs.i + 1 if hasattr(dfs, 'i') else 1
-
-        print(dfs.i, cur)
-
-    for v in closed_valves - set(cur):
-        print(cur, 'v=', v)
-        if t1 > dist[v1][v] + 1:
-            t = t1 - dist[v1][v] - 1
-            dfs(cur + (v,), t, closed_valves)
+@functools.cache
+def max_flow1(v: int, t: int, remaining: frozenset[int]) -> int:
+    _max_flow = 0
+    for u in remaining:
+        if (tt := t - dist[u][v] - 1) > 0:
+            flow = graph[u]['flow_rate'] * tt + max_flow1(u, tt, remaining - {u}, )
+            _max_flow = max(_max_flow, flow)
+    return _max_flow
 
 
-def puzzle31(input_file):
-    global dist, flow_rates
-    valves, flow_rates, dist = parse_input(input_file)
-    closed_valves = {i for i in range(len(valves)) if flow_rates[i] > 0}
-
-    print(closed_valves)
-    # ans = max_flow(valves.index('AA'), 30, closed_valves)
-    dfs((valves.index('AA'),), 30, closed_valves)
-    return
+def max_flow2(v: int, t: int, remaining: frozenset[int]) -> int:
+    _max_flow = max_flow1(v0, t0, remaining)
+    for u in remaining:
+        if (tt := t - dist[u][v] - 1) > 0:
+            flow = graph[u]['flow_rate'] * tt + max_flow2(u, tt, remaining - {u}, )
+            _max_flow = max(_max_flow, flow)
+    return _max_flow
 
 
-# print('Day #16, Part One:', puzzle31('day16.txt'))
-print('Day #16, Part One:', puzzle31('input.txt'))
+def puzzle31(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+
+    global graph, dist, v0, t0
+    graph = parse_input(data)
+    dist = floyd_warshall(graph)
+    v0, t0 = 0, 30
+    while graph[v0]['name'] != 'AA':
+        v0 += 1
+
+    valves = frozenset(i for i in range(len(graph)) if graph[i]['flow_rate'] > 0)
+
+    return max_flow1(v0, t0, valves, )
+
+
+def puzzle32(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+
+    global graph, dist, v0, t0
+    graph = parse_input(data)
+    dist = floyd_warshall(graph)
+    v0, t0 = 0, 26
+    while graph[v0]['name'] != 'AA':
+        v0 += 1
+
+    valves = frozenset(i for i in range(len(graph)) if graph[i]['flow_rate'] > 0)
+
+    return max_flow2(v0, t0, valves, )
+
+
+if __name__ == '__main__':
+    print('Day #16, part one:', puzzle31('./input/day16.txt'))
+    timestamp = time.time()
+    print('Day #16, part two:', puzzle32('./input/day16.txt'), f' (runtime: {round(time.time() - timestamp, 1)}s) ')
