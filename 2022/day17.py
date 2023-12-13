22ab9cabbf1b9cd091db1986fc153499e0ec1247
ehsan <enyazdi@gmail.com>
2023-12-13 10:34:29 +0330

    2022 day17 refactored

--- 2022/day17.py
+++ 2022/day17.py
@@ -1,118 +1,118 @@
-import fileHandle, re
+from itertools import cycle
+from typing import Generator
+
+import file_handle
 
 
-def check_under(rock, x, y, chamber):
-    # print(x, y)
-    for pixel in rock:
-        xu = pixel[0] + x
-        yu = pixel[1] + y - 1
-        if yu < len(chamber) and chamber[yu][xu] == 1:
-            return True
-    return False
-
-
-def draw():
-    print()
-    for i in range(len(chamber) - 1, -1, -1):
-        for j in chamber[i]:
-            if j:
-                print('#', end='')
-            else:
-                print('.', end='')
+def plot():
+    for row in reversed(chamber):
+        for cell in row:
+            print('#' if cell else '.', end='')
         print()
 
 
-def in_range(x, y):
-    return 0 <= x <= 6 and y < len(chamber)
+global chamber, jet_index
 
 
-def gas_push(rock, x, y):
-    global g, gas
-    g = (g + 1) % len(gas)
+def rock_generator() -> Generator:
+    rocks = [
+        {(0, 0), (1, 0), (2, 0), (3, 0)},  # －
+        {(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)},  # ➕
+        {(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)},  # ᒧ
+        {(0, 0), (0, 1), (0, 2), (0, 3)},  # |
+        {(0, 0), (1, 0), (0, 1), (1, 1)},  # ◼
+    ]
+    heights = [1, 3, 3, 4, 2, ]
+    for i in cycle(range(len(rocks))):
+        yield rocks[i], heights[i]
 
-    if gas[g] == '<':
-        for pixel in rock:
-            if not (0 <= x - 1 + pixel[0] <= 6) or in_range(x - 1 + pixel[0], y + pixel[1]) and chamber[y + pixel[1]][
-                x - 1 + pixel[0]] == 1:
-                break
-        else:
-            x -= 1
-    if gas[g] == '>':
-        for pixel in rock:
-            # print(x + 1 + pixel[0])
-            if not (0 <= x + 1 + pixel[0] <= 6) or in_range(x + 1 + pixel[0], y + pixel[1]) and chamber[y + pixel[1]][
-                x + 1 + pixel[0]] == 1:
-                break
-        else:
-            x += 1
+
+def jet_generator(jet_pattern: str) -> Generator:
+    global jet_index
+    jet_index = -1
+    dx = {'<': -1, '>': 1}
+    for ch in cycle(jet_pattern):
+        jet_index = (jet_index + 1) % len(jet_pattern)
+        yield dx[ch]
+
+
+def jet_push(x: int, y: int, rock: set[tuple], dx: int) -> int:
+    if all(0 <= x + rx + dx <= 6 and not chamber[y + ry][x + rx + dx]
+           for (rx, ry) in rock):
+        x += dx
     return x
 
 
-def down(shape):
-    global top, g, gas, chamber
-    x = 2
-    y = top + 5
-    rock_shapes = ['-', '+', '_|', '|', 'o']
-    match rock_shapes[shape % len(rock_shapes)]:
-        case '-':
-            rock = [(0, 0), (1, 0), (2, 0), (3, 0)]
-            width = 4
-            height = 1
-        case '+':
-            rock = [(1, 2), (0, 1), (1, 1), (2, 1), (1, 0)]
-            width = 3
-            height = 3
-        case '_|':
-            rock = [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)]
-            width = 3
-            height = 3
-        case '|':
-            rock = [(0, 0), (0, 1), (0, 2), (0, 3)]
-            width = 1
-            height = 4
-        case 'o':
-            rock = [(0, 0), (1, 0), (0, 1), (1, 1)]
-            width = 2
-            height = 2
+def fall(top: int, falling_rock: Generator, jet: Generator) -> int:
+    global chamber
+    chamber += [[0] * 7 for _ in range(len(chamber), top + 8)]
+    rock, height = next(falling_rock)
 
-    while True:
+    y = top + 4
+    x = jet_push(2, y, rock, next(jet), )
+    while all(not chamber[y + ry - 1][x + rx] for (rx, ry) in rock):
         y -= 1
-        x = gas_push(rock, x, y)
-        if check_under(rock, x, y, chamber):
-            for l in range(top + 1, y + height):
-                chamber.append([0] * 7)
-            top = max(top, y + height - 1)
-            for pixel in rock:
-                # print(pixel, x, y, top)
-                chamber[y + pixel[1]][x + pixel[0]] = 1
-            break
+        x = jet_push(x, y, rock, next(jet), )
+
+    for (rx, ry) in rock:
+        chamber[y + ry][x + rx] = 1
+
+    return max(top, y + height - 1)
 
 
-def puzzle33(input_file):
-    data = fileHandle.readfile(input_file)
-    global gas, g, chamber, top
-    gas = data
-    g = -1
+def column_tops(top: int) -> list[int]:
+    global chamber
+    col_tops = [0] * len(chamber[0])
+    for x in range(len(col_tops)):
+        y = top
+        while not chamber[y][x]:
+            y -= 1
+        col_tops[x] = top - y
+
+    return col_tops
+
+
+def puzzle33(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+
+    global chamber
     chamber = [[1] * 7]
-    top, last_top = 0, 0
-    added = [0] * (10000 // 5)
-    for i in range(1600):
-        down(i)
-        if i + 1 in range(1600, 10000, 1725):
-            # added[i // 5] = top - last_top
-            print(top - last_top)
-            last_top = top
-    # for i in range(len(added) - 10):
-    #     if added[i] == 13 and added[i + 1] == 10 and added[i + 2] == 6:
-    #         print(i * 5)
+    falling_rock = rock_generator()
+    jet = jet_generator(data)
 
-    n = 1000000000000
-    a = (n - 1600) // 1725
-    b = (n - 431) % 1725
-    print(n, a, b)
-    ans = top + a * 2734
-    return ans
+    top = 0
+    for _ in range(2022):
+        top = fall(top, falling_rock, jet)
+
+    return top
 
 
-print('Day #17, Part One:', puzzle33('day17.txt'))
-# print('Day #17, Part One:', puzzle33('input.txt'))
+def puzzle34(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+
+    global chamber, jet_index
+    chamber = [[1] * 7]
+    falling_rock = rock_generator()
+    jet = jet_generator(data)
+
+    steps, top = 1000000000000, 0
+    patterns, saved_tops = [], []
+
+    for i in range(steps):
+        if i % 5 == 0:
+            pattern = (jet_index, *column_tops(top))
+            if pattern in patterns:
+                index = patterns.index(pattern)
+                period = i - index * 5
+                offset = top - saved_tops[index]
+                return (steps // period) * offset + saved_tops[(steps % period) // 5]
+
+            patterns.append(pattern)
+            saved_tops.append(top)
+
+        top = fall(top, falling_rock, jet)
+
+
+if __name__ == '__main__':
+    print('Day #17, part one:', puzzle33('./input/day17.txt'))
+    print('Day #17, part two:', puzzle34('./input/day17.txt'))
