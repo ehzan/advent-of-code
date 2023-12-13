c961ac617c31ddbc6a7fb854e2ffd219362cbb32
ehsan <enyazdi@gmail.com>
2023-12-13 10:57:12 +0330

    2022 day18 refactored

--- 2022/day18.py
+++ 2022/day18.py
@@ -1,69 +1,64 @@
-import fileHandle
+import file_handle
 
 
-def parse_input(input_file):
-    data = fileHandle.readfile(input_file).splitlines()
-    cubes = [line.split(',') for line in data]
-    cubes = [(int(c[0]) + 1, int(c[1]) + 1, int(c[2]) + 1) for c in cubes]
-    return cubes
+def parse_input(data: str) -> set[tuple]:
+    return {tuple(map(int, row.split(','))) for row in data.splitlines()}
 
 
-def are_adjacent(c1, c2):
-    return abs(c1[0] - c2[0]) + abs(c1[1] - c2[1]) + abs(c1[2] - c2[2]) == 1
+DIRECTIONS = {
+    (-1, 0, 0), (1, 0, 0),
+    (0, -1, 0), (0, 1, 0),
+    (0, 0, -1), (0, 0, 1),
+}
 
 
-def puzzle35(input_file):
-    cubes = parse_input(input_file)
-    exposed_sides = 0
-    for i in range(len(cubes)):
-        exposed_sides += 6
-        for j in range(0, i):
-            if are_adjacent(cubes[i], cubes[j]):
-                exposed_sides -= 2
+def adjacency(cube: tuple[int, ...]) -> set[tuple]:
+    (x, y, z) = cube
+    return {(x + dx, y + dy, z + dz) for (dx, dy, dz) in DIRECTIONS}
+
+
+def is_inside_boundary(cube: tuple[int, ...], bounds: list[tuple]) -> bool:
+    return all(min_ <= cord <= max_
+               for cord, min_, max_ in zip(cube, bounds[0], bounds[1], ))
+
+
+def puzzle35(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+    cubes = parse_input(data)
+
+    exposed_sides = len(cubes) * 6
+    for cube in cubes:
+        for adjacent_cube in adjacency(cube):
+            exposed_sides -= adjacent_cube in cubes
     return exposed_sides
 
 
-def adjacents(cube, cubes):
-    if not hasattr(adjacents, 'max_x'):
-        adjacents.max_x = max(cubes, key=lambda c: c[0])[0] + 1
-    if not hasattr(adjacents, 'max_y'):
-        adjacents.max_y = max(cubes, key=lambda c: c[1])[1] + 1
-    if not hasattr(adjacents, 'max_z'):
-        adjacents.max_z = max(cubes, key=lambda c: c[2])[2] + 1
-    x, y, z = cube
-    _adjacents = set()
-    if 0 < x:
-        _adjacents.add((x - 1, y, z))
-    if x < adjacents.max_x:
-        _adjacents.add((x + 1, y, z))
-    if 0 < y:
-        _adjacents.add((x, y - 1, z))
-    if y < adjacents.max_y:
-        _adjacents.add((x, y + 1, z))
-    if 0 < z:
-        _adjacents.add((x, y, z - 1))
-    if z < adjacents.max_z:
-        _adjacents.add((x, y, z + 1))
+def puzzle36(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+    cubes = parse_input(data)
 
-    return _adjacents
+    bounds = [tuple(min(cords) - 1 for cords in zip(*cubes)),
+              tuple(max(cords) + 1 for cords in zip(*cubes)), ]
 
-
-def puzzle36(input_file):
-    cubes = parse_input(input_file)
     wet_sides = 0
-    boundary = {(0, 0, 0)}
-    reached = {(0, 0, 0)}
-    while boundary:
-        cube = boundary.pop()
-        for a in adjacents(cube, cubes):
-            if a not in reached:
-                if a in cubes:
+    outside = {bounds[0]}
+    visited = set()
+    while outside:
+        cube = outside.pop()
+        if cube in visited:
+            continue
+        visited.add(cube)
+
+        for adjacent_cube in adjacency(cube):
+            if is_inside_boundary(adjacent_cube, bounds):
+                if adjacent_cube in cubes:
                     wet_sides += 1
                 else:
-                    boundary.add(a)
-                    reached.add(a)
+                    outside.add(adjacent_cube)
+
     return wet_sides
 
 
-print('Day #18, Part One:', puzzle35('day18.txt'))
-print('Day #18, Part Two:', puzzle36('day18.txt'))
+if __name__ == '__main__':
+    print('Day #18, part one:', puzzle35('./input/day18.txt'))
+    print('Day #18, part two:', puzzle36('./input/day18.txt'))
