263bb5161734a760119f01722e88c35cbca31809
ehsan <enyazdi@gmail.com>
2024-12-11 21:29:23 +0330

    2024 day9 refactored

--- 2024/day9.py
+++ 2024/day9.py
@@ -1,3 +1,4 @@
+import heapq
 from collections import defaultdict
 
 import file_handle
@@ -27,7 +28,7 @@ def puzzle17(input_file: str) -> int:
         file = files[file_id]
         for free_space in free_spaces[free_space_index:file_id]:
             if free_space[1] < file[1]:
-                offset, size = free_space[0], free_space[1]
+                offset, size = free_space
                 checksum += file_id * (offset * size + size * (size - 1) // 2)
                 file[1] -= size
                 free_space_index += 1
@@ -44,7 +45,7 @@ def puzzle17(input_file: str) -> int:
     return checksum
 
 
-def puzzle18(input_file: str) -> int:
+def puzzle18_slow(input_file: str) -> int:
     data = file_handle.readfile(input_file).strip()
     files, free_spaces = parse_input(data)
 
@@ -60,7 +61,38 @@ def puzzle18(input_file: str) -> int:
 
     checksum = sum(file_id * (offset * size + size * (size - 1) // 2)
                    for file_id, [offset, size] in files.items())
+    return checksum
 
+
+def free_spaces_as_dict(free_spaces: list[list[tuple[int, int]]]) -> dict[int, list[int]]:
+    free_dict = defaultdict(list)
+    for free_space in free_spaces:
+        free_dict[free_space[1]].append(free_space[0])
+    for offset_list in free_dict.values():
+        heapq.heapify(offset_list)
+    return free_dict
+
+
+def puzzle18(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+    files, free_spaces = parse_input(data)
+
+    free_dict = free_spaces_as_dict(free_spaces)
+
+    for file_id in range(len(files) - 1, -1, -1):
+        file = files[file_id]
+        file_offset, file_size = file
+        candidates = [(offset_list[0], free_size)
+                      for free_size, offset_list in free_dict.items()
+                      if free_size >= file_size and offset_list and offset_list[0] < file_offset]
+        if candidates:
+            (free_offset, free_size) = min(candidates)
+            file[0] = free_offset
+            heapq.heappop(free_dict[free_size])
+            heapq.heappush(free_dict[free_size - file_size], free_offset + file_size)
+
+    checksum = sum(file_id * (offset * size + size * (size - 1) // 2)
+                   for file_id, [offset, size] in files.items())
     return checksum
 
 
